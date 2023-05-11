345bb3540b56638e2464fae4e3eaea100a175c3d
achmadbima704 <achmadbima704@gmail.com>
2023-05-11 12:22:10 +0700

    testing 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/testing', [\App\Http\Controllers\SiteController::class, 'index']);
